ea8acaee6dc12f90a5100488509117d0cf1b5af2
kxk8 <arkd112008@gmail.com>
2025-05-20 20:14:09 +0300

    Promo_post_format.py

---
+++ Promo_post_format.py
@@ -0,0 +1,11 @@
+def format_promo_post(gift_text, participants_count, giveaway_link):
+    return f"""
+🎁 *سحب جديد في روليت G.T!*
+
+{gift_text}
+
+👥 عدد المشاركين حتى الآن: {participants_count}
+🕓 سارع بالمشاركة قبل انتهاء الوقت!
+
+👇 اضغط الزر أدناه للمشاركة:
+""", giveaway_link
