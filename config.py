2b237f64b4ab33d91734ac78ee3ce77dccad40e3
kxk8 <arkd112008@gmail.com>
2025-05-20 19:44:23 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# عنوان محفظتك TON
+TON_WALLET_ADDRESS = "UQCGQxKYomJG3L1CJUw5C96yIWzzPaqHjMQd3r9G3usCCm1a"
+
+# قناة الترويج الرئيسية
+PROMO_CHANNEL_USERNAME = "xxd26"
+
+# ID المطور المسؤول عن البوت (لتنبيهات الإدارة)
+ADMIN_ID = 123456789
